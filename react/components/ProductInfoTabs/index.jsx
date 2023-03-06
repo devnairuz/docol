9c7602b6e8312ff6fc83d32a587387c8d877b572
claudimar <claudimar.bezerra@nairuz.com.br>
2023-03-06 08:28:34 -0300

    Ajuste componente ProductInfoTabs

--- react/components/ProductInfoTabs/index.jsx
+++ react/components/ProductInfoTabs/index.jsx
@@ -13,55 +13,47 @@ const ProductInfoTabs = ({ children }) => {
     setActive(properties[0].name)
   }, [])
 
-  if(!productInfos) {
-    return
-  }
-
   return (
-    <>
-    {productInfos && (
-      <div className={styles.productInfoTabs}>
-        <div className={styles.tabHeader}>
-          {productInfos.map((info, index) => (
-            <button
-              type="button"
-              onClick={() => setActive(info.name)}
-              key={info.name}
-              data-control-for={info.name}
-              className={`${styles.infoControl}${active === info.name ? ` ${styles.active}`:''}`}>{info.name}</button>
-          ))}
-
-          {children && (
-            <button
-              type="button"
-              onClick={() => setActive('Avaliações')}
-              key="Avaliações"
-              data-control-for="Avaliações"
-              className={`${styles.infoControl}${active === "Avaliações" ? ` ${styles.active}`:''}`}>Avaliações</button>
-          )}
-        </div>
-
+    <div className={styles.productInfoTabs}>
+      <div className={styles.tabHeader}>
         {productInfos.map((info, index) => (
-          <div
-            className={`${styles.tabContent}${active === info.name ? ` ${styles.active}`:''}`}
-            key={index}
-            data-content-for={info.name}
-            dangerouslySetInnerHTML={{__html: info.values}}
-          />
+          <button
+            type="button"
+            onClick={() => setActive(info.name)}
+            key={info.name}
+            data-control-for={info.name}
+            className={`${styles.infoControl}${active === info.name ? ` ${styles.active}`:''}`}>{info.name}</button>
         ))}
 
         {children && (
-        <div
-          className={`${styles.tabContent}${active === "Avaliações" ? ` ${styles.active}`:''}`}
-          key="Avaliações"
-          data-content-for="Avaliações"
-        >
-            {children}
-        </div>
+          <button
+            type="button"
+            onClick={() => setActive('Avaliações')}
+            key="Avaliações"
+            data-control-for="Avaliações"
+            className={`${styles.infoControl}${active === "Avaliações" ? ` ${styles.active}`:''}`}>Avaliações</button>
         )}
       </div>
-    )}
-    </>
+
+      {productInfos.map((info, index) => (
+        <div
+          className={`${styles.tabContent}${active === info.name ? ` ${styles.active}`:''}`}
+          key={index}
+          data-content-for={info.name}
+          dangerouslySetInnerHTML={{__html: info.values}}
+        />
+      ))}
+
+      {children && (
+      <div
+        className={`${styles.tabContent}${active === "Avaliações" ? ` ${styles.active}`:''}`}
+        key="Avaliações"
+        data-content-for="Avaliações"
+      >
+          {children}
+      </div>
+      )}
+    </div>
   )
 }
 
