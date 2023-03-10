a2f972269e2ccfc1192e18dc4aa0791899bc3fe5
claudimar <claudimar.bezerra@nairuz.com.br>
2023-03-10 10:04:09 -0300

    Correção componente

--- react/components/ProductInfoTabs/index.jsx
+++ react/components/ProductInfoTabs/index.jsx
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useProduct } from 'vtex.product-context';
 
-import styles from "./productInfoTabs.css"
+import styles from './productInfoTabs.css'
 
 const ProductInfoTabs = ({ children }) => {
-  const { product } = useProduct();
+  const { product, loadingItem } = useProduct();
   const [active, setActive] = useState('');
   const [productInfos, setProductInfos] = useState([]);
   const [openTabHeader, setOpenTabHeader] = useState(false);
   
   useEffect(() => {
-    setProductInfos(product?.properties)
-
-    setActive(product?.properties[0].name)
+    if(product && product.properties.length > 0) {
+      setProductInfos(product.properties)
+      setActive(product.properties[0].name)
+    } else {
+      setActive('Avaliações')
+    }
   }, [product])
 
   const handleBtnControl = (name) => {
@@ -20,48 +23,79 @@ const ProductInfoTabs = ({ children }) => {
 
     setOpenTabHeader(!openTabHeader)
   }
+  
+  if (loadingItem) return
+
+  console.log(!!productInfos.length)
 
   return (
-    <div className={styles.productInfoTabs}>
-      <div className={openTabHeader ? styles.tabHeaderOpen : styles.tabHeader}>
+    <>
+    {productInfos.length > 0 ? (
+      <div className={styles.productInfoTabs}>
+        <div className={openTabHeader ? styles.tabHeaderOpen : styles.tabHeader}>
+          {productInfos?.map((info, index) => (
+            <button
+              type="button"
+              onClick={() => handleBtnControl(info.name)}
+              key={info.name}
+              data-control-for={info.name}
+              className={`${styles.infoControl}${active === info.name ? ` ${styles.active}`:''}`}>{info.name}</button>
+          ))}
+
+          {children && (
+            <button
+              type="button"
+              onClick={() => handleBtnControl('Avaliações')}
+              key="Avaliações"
+              data-control-for="Avaliações"
+              className={`${styles.infoControl}${active === "Avaliações" ? ` ${styles.active}`:''}`}>Avaliações</button>
+          )}
+        </div>
+
         {productInfos?.map((info, index) => (
-          <button
-            type="button"
-            onClick={() => handleBtnControl(info.name)}
-            key={info.name}
-            data-control-for={info.name}
-            className={`${styles.infoControl}${active === info.name ? ` ${styles.active}`:''}`}>{info.name}</button>
+          <div
+            className={`${styles.tabContent}${active === info.name ? ` ${styles.active}`:''}`}
+            key={index}
+            data-content-for={info.name}
+            dangerouslySetInnerHTML={{__html: info.values}}
+          />
         ))}
 
         {children && (
-          <button
-            type="button"
-            onClick={() => handleBtnControl('Avaliações')}
-            key="Avaliações"
-            data-control-for="Avaliações"
-            className={`${styles.infoControl}${active === "Avaliações" ? ` ${styles.active}`:''}`}>Avaliações</button>
+        <div
+          className={`${styles.tabContent}${active === "Avaliações" ? ` ${styles.active}`:''}`}
+          key="Avaliações"
+          data-content-for="Avaliações"
+        >
+          {children}
+        </div>
         )}
       </div>
+    ) : (
+      <div className={styles.productInfoTabs}>
+        <div className={openTabHeader ? styles.tabHeaderOpen : styles.tabHeader}>
+          {children && (
+            <button
+              type="button"
+              onClick={() => handleBtnControl('Avaliações')}
+              key="Avaliações"
+              data-control-for="Avaliações"
+              className={`${styles.infoControl}${active === "Avaliações" ? ` ${styles.active}`:''}`}>Avaliações</button>
+          )}
+        </div>
 
-      {productInfos?.map((info, index) => (
+        {children && (
         <div
-          className={`${styles.tabContent}${active === info.name ? ` ${styles.active}`:''}`}
-          key={index}
-          data-content-for={info.name}
-          dangerouslySetInnerHTML={{__html: info.values}}
-        />
-      ))}
-
-      {children && (
-      <div
-        className={`${styles.tabContent}${active === "Avaliações" ? ` ${styles.active}`:''}`}
-        key="Avaliações"
-        data-content-for="Avaliações"
-      >
-        {children}
+          className={`${styles.tabContent}${active === "Avaliações" ? ` ${styles.active}`:''}`}
+          key="Avaliações"
+          data-content-for="Avaliações"
+        >
+          {children}
+        </div>
+        )}
       </div>
-      )}
-    </div>
+    )}
+    </>
   )
 }
 
